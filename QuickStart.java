b48a63eeb8ba599ec115030eb0fba23beb150140
syf <2654525303@qq.com>
2020-08-30 20:20:45 +0800

    keep JUnit stuff

--- QuickStart.java
+++
@@ -1,9 +0,0 @@
-import leetcode.core.Congratulator;
-
-public class QuickStart
-{
-    public static void main(String[] args) {
-        System.out.println("Hello from VS Code Java");
-        Congratulator.yell();
-    }
-}
