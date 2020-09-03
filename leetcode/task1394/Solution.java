a1df5c2d7258a59baed2fcecb22edb32b1831102
syf <2654525303@qq.com>
2020-09-03 11:55:14 +0800

    solve 1394

--- leetcode/task1394/Solution.java
+++ leetcode/task1394/Solution.java
@@ -1,10 +1,24 @@
 package leetcode.task1394;
 
+import java.util.TreeMap;
+
 /**
  * @see https://leetcode-cn.com/problems/find-lucky-integer-in-an-array/
  */
 public class Solution {
     public int findLucky(int[] arr) {
-        return 0;
+        int[] result = { -1 };
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.merge(arr[i], 1, Integer::sum);
+        }
+        map.forEach((num, count) -> {
+            if (num == count) {
+                result[0] = num; // TreeMap iteration default as low -> ... -> high
+            }
+        });
+
+        return result[0];
     }
 }
