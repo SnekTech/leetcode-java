24766584f5a077568066f08d542822a02cf11ca7
syf <2654525303@qq.com>
2020-09-11 23:04:09 +0800

    slowly solve task 409

--- leetcode/task409/Solution.java
+++ leetcode/task409/Solution.java
@@ -1,11 +1,30 @@
 package leetcode.task409;
 
+import java.util.ArrayList;
+
 /**
  * @see https://leetcode-cn.com/problems/longest-palindrome/
  */
 public class Solution {
     public int longestPalindrome(String s) {
         int result = 0;
+        
+        ArrayList<Character> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (list.indexOf(current) != -1) {
+                list.remove(list.indexOf(current));
+                result++;
+            } else {
+                list.add(current);
+            }
+        }
+
+        result*=2;
+        if (list.size() != 0) {
+            result++;
+        }
+
         return result;
     }
 }
