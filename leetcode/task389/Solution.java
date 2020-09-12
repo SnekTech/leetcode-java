5c448f4817637986dab28c9579ef4a9e5e5f2077
syf <2654525303@qq.com>
2020-09-12 22:46:04 +0800

    set up task 389

---
+++ leetcode/task389/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.task389;
+
+/**
+ * @see https://leetcode-cn.com/problems/find-the-difference/
+ */
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        char result = 'a';
+
+        return result;
+    }
+}
---
+++ leetcode/task389/Test1.java
@@ -0,0 +1,18 @@
+package leetcode.task389;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Test1 {
+    @Test
+    public void example() {
+        String s = "abcd";
+        String t = "abcde";
+
+        char expected = 'c';
+        char actual = new Solution().findTheDifference(s, t);
+
+        assertEquals(expected, actual);
+    }
+}
