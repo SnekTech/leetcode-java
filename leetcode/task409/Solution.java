786baafaea5b1d5b8f43c9dabd605d4252f38cf3
syf <2654525303@qq.com>
2020-09-12 17:08:03 +0800

    improve task 409

--- leetcode/task409/Solution.java
+++ leetcode/task409/Solution.java
@@ -1,32 +1,25 @@
 package leetcode.task409;
 
-import java.util.HashMap;
-
 /**
  * @see https://leetcode-cn.com/problems/longest-palindrome/
  */
 public class Solution {
     public int longestPalindrome(String s) {
-        int[] result = new int[] {0};
-        
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            map.merge(s.charAt(i), 1, Integer::sum);
+        int[] count = new int[128];
+
+        for (char c : s.toCharArray()) {
+            count[c]++;
         }
 
-        map.forEach((k, v) -> {
-            if (v >= 2) {
-                result[0] += v / 2;
-                map.put(k, v % 2);
+        int result = 0;
+        for (int n : count) {
+            result += n / 2 * 2;
+
+            if (n % 2 == 1 && result % 2 == 0) {
+                result++;
             }
-        });
-
-        result[0] *= 2;
-
-        if (map.containsValue(1)) {
-            result[0]++;
         }
 
-        return result[0];
+        return result;
     }
 }
