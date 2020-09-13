0021544d62a81a07e0be4cc8bcb60be1eccb7a01
syf <2654525303@qq.com>
2020-09-13 14:53:58 +0800

    solve task 605

--- leetcode/task605/Solution.java
+++ leetcode/task605/Solution.java
@@ -1,9 +1,41 @@
 package leetcode.task605;
 
+/**
+ * @see https://leetcode-cn.com/problems/can-place-flowers/
+ */
 public class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        boolean result = true;
+        if (flowerbed.length == 0) return false;
+        if (flowerbed.length == 1) {
+            return n == 0 || flowerbed[0] == 0;
+        }
 
-        return result;
+        int[] myFlowerbed = new int[flowerbed.length];
+        for (int i = 0; i < myFlowerbed.length; i++) {
+            myFlowerbed[i] = flowerbed[i];
+        }
+
+        for (int i = 0; i < myFlowerbed.length; i++) {
+            if (i > 0 && flowerbed[i - 1] == 1 || i < myFlowerbed.length - 1 && flowerbed[i + 1] == 1) {
+                myFlowerbed[i] = 1;
+            }
+        }
+
+        int canFlower = 0;
+        int zeroCounter = 0;
+        for (int i = 0; i < myFlowerbed.length; i++) {
+            if (myFlowerbed[i] == 0) {
+                zeroCounter++;
+            }
+            else {
+                if (zeroCounter != 0) {
+                    canFlower += (zeroCounter + 1) / 2;
+                    zeroCounter = 0;
+                }
+            }
+        }
+        canFlower += (zeroCounter + 1) / 2;
+
+        return n <= canFlower;
     }
 }
--- leetcode/task605/Test1.java
+++ leetcode/task605/Test1.java
@@ -6,12 +6,68 @@ import org.junit.Test;
 
 public class Test1 {
     @Test
-    public void example() {
+    public void example1() {
         int[] flowerbed = new int[] { 1, 0, 0, 0, 1 };
         int n = 1;
-        boolean expected = true;
 
+        boolean expected = true;
         boolean actual = new Solution().canPlaceFlowers(flowerbed, n);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void example2() {
+        int[] flowerbed = new int[] { 1, 0, 0, 0, 1 };
+        int n = 2;
+
+        boolean expected = false;
+        boolean actual = new Solution().canPlaceFlowers(flowerbed, n);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void wrong1() {
+        int[] flowerbed = new int[] { 1, 0, 0, 0, 0, 1 };
+        int n = 2;
+
+        boolean expected = false;
+        boolean actual = new Solution().canPlaceFlowers(flowerbed, n);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void wrong2() {
+        int[] flowerbed = new int[] { 1, 0, 0, 0, 1, 0, 0 };
+        int n = 2;
+
+        boolean expected = true;
+        boolean actual = new Solution().canPlaceFlowers(flowerbed, n);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void wrong3() {
+        int[] flowerbed = new int[] { 0 };
+        int n = 1;
+
+        boolean expected = true;
+        boolean actual = new Solution().canPlaceFlowers(flowerbed, n);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void wrong4() {
+        int[] flowerbed = new int[] { 1, 0 };
+        int n = 1;
+
+        boolean expected = false;
+        boolean actual = new Solution().canPlaceFlowers(flowerbed, n);
+
         assertEquals(expected, actual);
     }
 }
