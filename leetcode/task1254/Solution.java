95cfa1575b04563c59e4ad67fff0734976547622
syf <2654525303@qq.com>
2020-12-17 20:23:00 +0800

    set up task 1254

---
+++ leetcode/task1254/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.task1254;
+
+/**
+ * @see https://leetcode-cn.com/problems/number-of-closed-islands/
+ */
+public class Solution {
+    public int closedIsland(int[][] grid) {
+        return 0;
+    }
+}
---
+++ leetcode/task1254/Test1.java
@@ -0,0 +1,57 @@
+package leetcode.task1254;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Test1 {
+    @Test
+    public void example1() {
+        int[][] grid = new int[][] {
+            {1,1,1,1,1,1,1,0},
+            {1,0,0,0,0,1,1,0},
+            {1,0,1,0,1,1,1,0},
+            {1,0,0,0,0,1,0,1},
+            {1,1,1,1,1,1,1,0}
+        };
+
+        Solution solution = new Solution();
+        int expected = 2;
+        int actual = solution.closedIsland(grid);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void example2() {
+        
+        int[][] grid = new int[][] {
+            {0,0,1,0,0},
+            {0,1,0,1,0},
+            {0,1,1,1,0}
+        };
+
+        Solution solution = new Solution();
+        int expected = 1;
+        int actual = solution.closedIsland(grid);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void example3() {
+        
+        int[][] grid = new int[][] {
+            {1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,1},
+            {1,0,1,1,1,0,1},
+            {1,0,1,0,1,0,1},
+            {1,0,1,1,1,0,1},
+            {1,0,0,0,0,0,1},
+            {1,1,1,1,1,1,1}
+        };
+
+        Solution solution = new Solution();
+        int expected = 1;
+        int actual = solution.closedIsland(grid);
+        assertEquals(expected, actual);
+    }
+}
