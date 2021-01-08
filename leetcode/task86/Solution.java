99ec4426cc975eb695b3a89f7dc7692cb7f8f115
syf <2654525303@qq.com>
2021-01-08 16:48:09 +0800

    finish task 86

--- leetcode/task86/Solution.java
+++ leetcode/task86/Solution.java
@@ -14,6 +14,26 @@ import leetcode.core.ListNode;
  */
 public class Solution {
     public ListNode partition(ListNode head, int x) {
-        return null;
+        var dummy1 = new ListNode(-1, null);
+        var dummy2 = new ListNode(-1, null);
+        var p1 = dummy1;
+        var p2 = dummy2;
+
+        var p = head;
+        while (p != null) {
+            if (p.val < x) {
+                p1.next = new ListNode(p.val, p1.next);
+                p1 = p1.next;
+            }
+            else {
+                p2.next = new ListNode(p.val, p2.next);
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+
+        p1.next = dummy2.next;
+
+        return dummy1.next;
     }
 }
