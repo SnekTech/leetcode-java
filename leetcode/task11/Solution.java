097068444eed7be3d87b4bfcb5ba4001e4068734
syf <2654525303@qq.com>
2021-01-20 18:47:52 +0800

    finish task 11

--- leetcode/task11/Solution.java
+++ leetcode/task11/Solution.java
@@ -5,6 +5,29 @@ package leetcode.task11;
  */
 public class Solution {
     public int maxArea(int[] height) {
-        return 0;
+        return maxSubArea(height, height.length - 1);
+    }
+
+    /**
+     * find max from sub area [0 ... end]
+     * @param height original height data
+     * @param end end index (inclusive)
+     * @return max from sub area
+     */
+    private int maxSubArea(int[] height, int end) {
+        if (end <= 0) return 0;
+
+        if (end == 1) {
+            return Math.min(height[0], height[1]);
+        }
+
+        int max = maxSubArea(height, end - 1);
+        for (int i = 0; i < end; i++) {
+            int w = end - i;
+            int h = Math.min(height[i], height[end]);
+            max = Math.max(max, w * h);
+        }
+
+        return max;
     }
 }
