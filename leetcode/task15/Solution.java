77fc581e9a053f2433e12fa218c615487efcad28
syf <2654525303@qq.com>
2021-01-21 23:58:25 +0800

    finish task 15

--- leetcode/task15/Solution.java
+++ leetcode/task15/Solution.java
@@ -1,5 +1,7 @@
 package leetcode.task15;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -7,6 +9,42 @@ import java.util.List;
  */
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        return null;
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int first = 0; first < n; first++) {
+            if (first >0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+
+            int third = n - 1;
+            int target = -nums[first];
+
+            for (int second = first + 1; second < n; second++) {
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+
+                // 保证 second 在 third 左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    third--;
+                }
+
+                if (second == third) {
+                    break;
+                }
+
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+
+        return ans;
     }
 }
