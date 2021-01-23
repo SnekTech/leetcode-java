4575e4cdfc512d555ea56a1a74e676e0a88513e9
syf <2654525303@qq.com>
2021-01-23 14:45:45 +0800

    finish task 18

--- leetcode/task18/Solution.java
+++ leetcode/task18/Solution.java
@@ -1,5 +1,7 @@
 package leetcode.task18;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -7,6 +9,48 @@ import java.util.List;
  */
 public class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+
+        Arrays.sort(nums);
+        for (int first = 0; first < n; first++) {
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+
+            for (int second = first + 1; second < n; second++) {
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+
+                for (int third = second + 1; third < n; third++) {
+                    if (third > second + 1 && nums[third] == nums[third - 1]) {
+                        continue;
+                    }
+
+                    int forth = n - 1;
+                    while (third < forth) {
+                        int sum = nums[first] + nums[second] + nums[third] + nums[forth];
+                        if (sum < target) {
+                            third++;
+                        }
+                        else if (sum > target) {
+                            forth--;
+                        }
+                        else {
+                            List<Integer> list = new ArrayList<>();
+                            list.add(nums[first]);
+                            list.add(nums[second]);
+                            list.add(nums[third]);
+                            list.add(nums[forth]);
+                            result.add(list);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 }
