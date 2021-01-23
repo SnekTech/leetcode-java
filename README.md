1b8234dd9d0fcd851aa7cfcb0507039952a30398
syf <2654525303@qq.com>
2021-01-23 15:00:16 +0800

    optimize task 18

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ LeetCode(CN) solutions written in Java11.
 - [x] [15](leetcode/task15/Solution.java)
 - [x] [16](leetcode/task16/Solution.java)
 - [x] [17](leetcode/task17/Solution.java)
-- [x] [18](leetcode/task18/Solution.java) tbo
+- [x] [18](leetcode/task18/Solution.java)
 - [x] [47](leetcode/task47/Solution.java) tbo
 - [x] [39](leetcode/task39/Solution.java)
 - [x] [40](leetcode/task40/Solution.java)
--- leetcode/task18/Solution.java
+++ leetcode/task18/Solution.java
@@ -10,42 +10,60 @@ import java.util.List;
 public class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> result = new ArrayList<>();
+
+        if (nums == null || nums.length < 4) {
+            return result;
+        }
+
         int n = nums.length;
 
         Arrays.sort(nums);
-        for (int first = 0; first < n; first++) {
+        for (int first = 0; first < n - 3; first++) {
             if (first > 0 && nums[first] == nums[first - 1]) {
                 continue;
             }
 
-            for (int second = first + 1; second < n; second++) {
+            if (nums[first] + nums[first + 1] + nums[first + 2] + nums[first + 3] > target) {
+                break;
+            }
+
+            if (nums[first] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target) {
+                continue;
+            }
+
+            for (int second = first + 1; second < n - 2; second++) {
                 if (second > first + 1 && nums[second] == nums[second - 1]) {
                     continue;
                 }
 
-                for (int third = second + 1; third < n; third++) {
-                    if (third > second + 1 && nums[third] == nums[third - 1]) {
-                        continue;
-                    }
+                if (nums[first] + nums[second] + nums[second + 1] + nums[second + 2] > target) {
+                    break;
+                }
 
-                    int forth = n - 1;
-                    while (third < forth) {
-                        int sum = nums[first] + nums[second] + nums[third] + nums[forth];
-                        if (sum < target) {
-                            third++;
+                if (nums[first] + nums[second] + nums[n - 2] + nums[n - 1] < target) {
+                    continue;
+                }
+
+                int left = second + 1, right = n - 1;
+                while (left < right) {
+                    int a = nums[first], b = nums[second], c = nums[left], d = nums[right];
+                    int sum = a + b + c + d;
+                    if (sum == target) {
+                        result.add(Arrays.asList(a, b, c, d));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
                         }
-                        else if (sum > target) {
-                            forth--;
+                        left++;
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
                         }
-                        else {
-                            List<Integer> list = new ArrayList<>();
-                            list.add(nums[first]);
-                            list.add(nums[second]);
-                            list.add(nums[third]);
-                            list.add(nums[forth]);
-                            result.add(list);
-                            break;
-                        }
+                        right--;
+                    }
+                    else if (sum < target) {
+                        left++;
+                    }
+                    else {
+                        right--;
                     }
                 }
             }
