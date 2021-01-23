c3f4f350c6fe3cd21d69a745c0d5190f5c871413
syf <2654525303@qq.com>
2021-01-23 16:40:56 +0800

    finish task 22

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ LeetCode(CN) solutions written in Java11.
 - [x] [16](leetcode/task16/Solution.java)
 - [x] [17](leetcode/task17/Solution.java)
 - [x] [18](leetcode/task18/Solution.java)
-- [ ] [22](leetcode/task22/Solution.java)
+- [x] [22](leetcode/task22/Solution.java) tbo
 - [x] [47](leetcode/task47/Solution.java) tbo
 - [x] [39](leetcode/task39/Solution.java)
 - [x] [40](leetcode/task40/Solution.java)
--- leetcode/task22/Solution.java
+++ leetcode/task22/Solution.java
@@ -1,9 +1,60 @@
 package leetcode.task22;
 
-import java.util.List;
+import java.util.*;
 
+/**
+ * @see <a href="https://leetcode-cn.com/problems/generate-parentheses/">link</a>
+ */
 public class Solution {
+    Set<String> result = new HashSet<>();
+    StringBuilder seq = new StringBuilder();
+    int length;
     public List<String> generateParenthesis(int n) {
-        return null;
+        length = 2 * n;
+        backTrack(0);
+        return new ArrayList<>(result);
+    }
+
+    private void backTrack(int i) {
+        if (i == length) {
+            if (parenthesisMatches(seq.toString())) {
+                result.add(seq.toString());
+            }
+            return;
+        }
+
+        seq.append('(');
+        backTrack(i + 1);
+        seq.setCharAt(seq.length() - 1, ')');
+        backTrack(i + 1);
+        seq.deleteCharAt(seq.length() - 1);
+    }
+
+    private static boolean parenthesisMatches(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            }
+            else if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                else {
+                    stack.pop();
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        var s = new Solution();
+        s.generateParenthesis(3);
     }
 }
