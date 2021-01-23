dc5d4c5465d6cf3c88130b70d3387a096cf165bf
syf <2654525303@qq.com>
2021-01-23 16:47:15 +0800

    optimize task 22

--- leetcode/task22/Solution.java
+++ leetcode/task22/Solution.java
@@ -8,53 +8,29 @@ import java.util.*;
 public class Solution {
     Set<String> result = new HashSet<>();
     StringBuilder seq = new StringBuilder();
-    int length;
+    int n;
     public List<String> generateParenthesis(int n) {
-        length = 2 * n;
-        backTrack(0);
+        this.n = n;
+        backTrack(0, 0);
         return new ArrayList<>(result);
     }
 
-    private void backTrack(int i) {
-        if (i == length) {
-            if (parenthesisMatches(seq.toString())) {
-                result.add(seq.toString());
-            }
+    private void backTrack(int open, int close) {
+        if (seq.length() == n * 2) {
+            result.add(seq.toString());
             return;
         }
 
-        seq.append('(');
-        backTrack(i + 1);
-        seq.setCharAt(seq.length() - 1, ')');
-        backTrack(i + 1);
-        seq.deleteCharAt(seq.length() - 1);
-    }
-
-    private static boolean parenthesisMatches(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                stack.push(c);
-            }
-            else if (c == ')') {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                else {
-                    stack.pop();
-                }
-            }
-            else {
-                return false;
-            }
+        if (open < n) {
+            seq.append('(');
+            backTrack(open + 1, close);
+            seq.deleteCharAt(seq.length() - 1);
         }
 
-        return stack.isEmpty();
-    }
-
-    public static void main(String[] args) {
-        var s = new Solution();
-        s.generateParenthesis(3);
+        if (close < open) {
+            seq.append(')');
+            backTrack(open, close + 1);
+            seq.deleteCharAt(seq.length() - 1);
+        }
     }
 }
