76602387a2d05343e9a8fbb74bedb8c4b9806e88
syf <2654525303@qq.com>
2021-01-23 14:19:43 +0800

    set up task 18

---
+++ leetcode/task18/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.task18;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/4sum/">link</a>
+ */
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        return null;
+    }
+}
