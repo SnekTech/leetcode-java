28ac056fbba4986bbf8e3e6908b4983b8f0d3b7d
syf <2654525303@qq.com>
2021-01-26 16:19:38 +0800

    set up task 49

---
+++ leetcode/task49/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.task49;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/group-anagrams/">link</a>
+ */
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return null;
+    }
+}
