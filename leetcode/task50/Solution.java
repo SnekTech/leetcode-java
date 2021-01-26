8175c77bb1e1279bd0173aa576c7e20d146a8ea9
syf <2654525303@qq.com>
2021-01-26 16:58:20 +0800

    set up task 50

---
+++ leetcode/task50/Solution.java
@@ -0,0 +1,7 @@
+package leetcode.task50;
+
+public class Solution {
+    public double myPow(double x, int n) {
+        return 0;
+    }
+}
---
+++ leetcode/task50/Test1.java
@@ -0,0 +1,37 @@
+package leetcode.task50;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test1 {
+    @Test
+    public void example1() {
+        double x = 2;
+        int n = 10;
+        Solution solution = new Solution();
+        double expected = 1024;
+        double actual = solution.myPow(x, n);
+        assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void example2() {
+        double x = 2.1;
+        int n = 3;
+        Solution solution = new Solution();
+        double expected = 9.261;
+        double actual = solution.myPow(x, n);
+        assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void example3() {
+        double x = 2;
+        int n = -2;
+        Solution solution = new Solution();
+        double expected = 0.25;
+        double actual = solution.myPow(x, n);
+        assertEquals(expected, actual, 0);
+    }
+}
