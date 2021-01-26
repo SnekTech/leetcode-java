a55945916d3230769d8dfc30110f27f2635fc353
syf <2654525303@qq.com>
2021-01-26 16:49:55 +0800

    finish task 49 - another approach

--- leetcode/task49/Solution.java
+++ leetcode/task49/Solution.java
@@ -8,18 +8,26 @@ import java.util.*;
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        for (var str : strs) {
-            char[] charArray = str.toCharArray();
-            Arrays.sort(charArray);
-            String key = new String(charArray);
-            if (map.containsKey(key)) {
-                map.get(key).add(str);
-            }
-            else {
-                List<String> list = new ArrayList<>();
-                list.add(str);
-                map.put(key, list);
-            }
+            int[] counts = new int[26];
+            for (var str : strs) {
+                Arrays.fill(counts, 0);
+
+                for (int i = 0; i < str.length(); i++) {
+                    counts[str.charAt(i) - 'a']++;
+                }
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < 26; i++) {
+                    if (counts[i] != 0) {
+                        sb.append((char)('a' + i));
+                        sb.append(counts[i]);
+                    }
+                }
+                String key = sb.toString();
+
+                List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
         }
 
         return new ArrayList<>(map.values());
