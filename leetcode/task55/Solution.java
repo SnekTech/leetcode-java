7bbfc56a73bcb0b3ddb4affae46fdca5aa5cf976
syf <2654525303@qq.com>
2021-01-27 17:18:30 +0800

    finish task 55

--- leetcode/task55/Solution.java
+++ leetcode/task55/Solution.java
@@ -5,6 +5,16 @@ package leetcode.task55;
  */
 public class Solution {
     public boolean canJump(int[] nums) {
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (i <= max) {
+                max = Math.max(max, i + nums[i]);
+                if (max >= nums.length - 1) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 }
--- leetcode/task55/Test1.java
+++ leetcode/task55/Test1.java
@@ -21,4 +21,20 @@ public class Test1 {
         boolean actual = solution.canJump(nums);
         assertFalse(actual);
     }
+
+    @Test
+    public void wrong1() {
+        int[] nums = new int[] { 3, 0, 0, 0 };
+        Solution solution = new Solution();
+        boolean actual = solution.canJump(nums);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void wrong2() {
+        int[] nums = new int[] { 0, 1 };
+        Solution solution = new Solution();
+        boolean actual = solution.canJump(nums);
+        assertFalse(actual);
+    }
 }
