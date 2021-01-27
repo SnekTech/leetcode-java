d4c0e4bb7f6d723f8018c8d25c1b4d3f3060bdcf
syf <2654525303@qq.com>
2021-01-27 19:47:18 +0800

    finish task 56

--- leetcode/task56/Solution.java
+++ leetcode/task56/Solution.java
@@ -1,10 +1,32 @@
 package leetcode.task56;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/merge-intervals/">link</a>
  */
 public class Solution {
     public int[][] merge(int[][] intervals) {
-        return null;
+        // 这题太数学了，我先抄了 😀
+
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval : intervals) {
+            int left = interval[0], right = interval[1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < left) {
+                merged.add(new int[]{left, right});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], right);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
     }
 }
