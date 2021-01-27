12026d572bce9c27f910206ea05bca15d691239a
syf <2654525303@qq.com>
2021-01-27 13:47:28 +0800

    finish task 54

--- README.md
+++ README.md
@@ -50,7 +50,7 @@ LeetCode(CN) solutions written in Java11.
 - [x] [47](leetcode/task47/Solution.java)
 - [x] [49](leetcode/task49/Solution.java)
 - [x] [50](leetcode/task50/Solution.java)
-- [ ] [54](leetcode/task54/Solution.java)
+- [x] [54](leetcode/task54/Solution.java)
 - [x] [86](leetcode/task86/Solution.java)
 - [x] [92](leetcode/task92/Solution.java)
 - [x] [189](leetcode/task189/Solution.java)
--- leetcode/task54/Solution.java
+++ leetcode/task54/Solution.java
@@ -1,5 +1,6 @@
 package leetcode.task54;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,6 +8,63 @@ import java.util.List;
  */
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+
+        if (matrix == null || matrix.length == 0) {
+            return  result;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int startI = 0, startJ = 0;
+        boolean verticalUnfinished = true, horizontalUnfinished = true;
+        while (verticalUnfinished && horizontalUnfinished) {
+            int i = startI, j = startJ;
+            while (j < n - startJ) {
+                result.add(matrix[i][j]);
+                j++;
+            }
+            j--;
+
+            i++;
+            if (i >= m - startI) {
+                break;
+            }
+
+            while (i < m - startI) {
+                result.add(matrix[i][j]);
+                i++;
+            }
+            i--;
+
+            j--;
+            if (j < startJ) {
+                break;
+            }
+
+            while (j >= startJ) {
+                result.add(matrix[i][j]);
+                j--;
+            }
+            j++;
+
+            i--;
+            if (i <= startI) {
+                break;
+            }
+
+            while (i > startI) {
+                result.add(matrix[i][j]);
+                i--;
+            }
+
+            startI++;
+            startJ++;
+            verticalUnfinished = m % 2 == 0 ? startI < m / 2 : startI <= m / 2;
+            horizontalUnfinished = n % 2 == 0 ? startJ < n / 2 : startJ <= n / 2;
+        }
+
+        return result;
     }
 }
