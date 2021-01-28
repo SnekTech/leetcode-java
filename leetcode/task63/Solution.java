0910ec72848433aa98ecea4c2cfb011981edcde8
syf <2654525303@qq.com>
2021-01-28 15:53:32 +0800

    set up task 63

---
+++ leetcode/task63/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.task63;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/unique-paths-ii/">link</a>
+ */
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        return 0;
+    }
+}
---
+++ leetcode/task63/Test1.java
@@ -0,0 +1,32 @@
+package leetcode.task63;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1 {
+    @Test
+    public void example1() {
+        int[][] obstacleGrid = new int[][] {
+                { 0, 0, 0 },
+                { 0, 1, 0 },
+                { 0, 0, 0 }
+        };
+        Solution solution = new Solution();
+        int expected = 2;
+        int actual = solution.uniquePathsWithObstacles(obstacleGrid);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void example2() {
+        int[][] obstacleGrid = new int[][] {
+                { 0, 1 },
+                { 0, 0 }
+        };
+        Solution solution = new Solution();
+        int expected = 1;
+        int actual = solution.uniquePathsWithObstacles(obstacleGrid);
+        assertEquals(expected, actual);
+    }
+}
