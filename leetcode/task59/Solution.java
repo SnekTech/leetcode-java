5b21e2cead93519f362d63d46a9985d4fcf9ea5c
syf <2654525303@qq.com>
2021-01-28 14:32:21 +0800

    finish task 59

--- leetcode/task59/Solution.java
+++ leetcode/task59/Solution.java
@@ -5,6 +5,51 @@ package leetcode.task59;
  */
 public class Solution {
     public int[][] generateMatrix(int n) {
-        return null;
+        int[][] matrix = new int[n][n];
+
+        int elem = 1, startI = 0, i, j;
+        boolean unfinished = true;
+
+        while (unfinished) {
+            i = startI;
+            j = startI;
+
+            while (j < n - startI) {
+                matrix[i][j] = elem++;
+                j++;
+            }
+            j--;
+
+            i++;
+            if (i >= n - startI) {
+                break;
+            }
+
+            while (i < n - startI) {
+                matrix[i][j] = elem++;
+                i++;
+            }
+            i--;
+
+            j--;
+
+            while (j >= startI) {
+                matrix[i][j] = elem++;
+                j--;
+            }
+            j++;
+
+            i--;
+
+            while (i > startI) {
+                matrix[i][j] = elem++;
+                i--;
+            }
+
+            startI++;
+            unfinished = n % 2 == 0 ? startI < n / 2 : startI <= n / 2;
+        }
+
+        return matrix;
     }
 }
