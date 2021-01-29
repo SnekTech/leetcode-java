d11c9e09dc568eb9154e7ceecb03bcd7fd228e6b
syf <2654525303@qq.com>
2021-01-29 17:22:38 +0800

    set up task 75

---
+++ leetcode/task75/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.task75;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/sort-colors/">link</a>
+ */
+public class Solution {
+    public void sortColors(int[] nums) {
+
+    }
+}
---
+++ leetcode/task75/Test1.java
@@ -0,0 +1,51 @@
+package leetcode.task75;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1 {
+    @Test
+    public void example1() {
+        int[] nums = new int[] { 2, 0, 2, 1, 1, 0 };
+
+        Solution solution = new Solution();
+        int[] expected = new int[] { 0, 0, 1, 1, 2, 2 };
+        solution.sortColors(nums);
+
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void example2() {
+        int[] nums = new int[] { 2, 0, 1 };
+
+        Solution solution = new Solution();
+        int[] expected = new int[] { 0, 1, 2 };
+        solution.sortColors(nums);
+
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void example3() {
+        int[] nums = new int[] { 0 };
+
+        Solution solution = new Solution();
+        int[] expected = new int[] { 0 };
+        solution.sortColors(nums);
+
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void example4() {
+        int[] nums = new int[] { 1 };
+
+        Solution solution = new Solution();
+        int[] expected = new int[] { 1 };
+        solution.sortColors(nums);
+
+        assertArrayEquals(expected, nums);
+    }
+}
