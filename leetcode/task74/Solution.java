7be1dd2a45c630d15adadd7b9a898fbe365259c0
syf <2654525303@qq.com>
2021-01-29 17:04:04 +0800

    set up task 74

---
+++ leetcode/task74/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.task74;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/search-a-2d-matrix/">link</a>
+ */
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return false;
+    }
+}
