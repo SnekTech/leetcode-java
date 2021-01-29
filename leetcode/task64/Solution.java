f897360e705c029ee75b9d99129859e0dbc1ea2e
syf <2654525303@qq.com>
2021-01-29 13:57:50 +0800

    finish task 64

--- leetcode/task64/Solution.java
+++ leetcode/task64/Solution.java
@@ -5,6 +5,29 @@ package leetcode.task64;
  */
 public class Solution {
     public int minPathSum(int[][] grid) {
-        return 0;
+        int m = grid.length, n = grid[0].length;
+        int[][] f = new int[m][n];
+        f[0][0] = grid[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                if (i == 0) {
+                    f[0][j] = f[0][j - 1] + grid[0][j];
+                    continue;
+                }
+                if (j == 0) {
+                    f[i][0] = f[i - 1][0] + grid[i][0];
+                    continue;
+                }
+
+                f[i][j] = Math.min(f[i][j - 1], f[i - 1][j]) + grid[i][j];
+            }
+        }
+
+        return f[m - 1][n - 1];
     }
 }
