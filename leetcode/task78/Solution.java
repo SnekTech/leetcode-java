d52dd137eb274e22458c70290cb4d3aeaf4aecec
syf <2654525303@qq.com>
2021-01-30 15:22:03 +0800

    finish task 78

--- leetcode/task78/Solution.java
+++ leetcode/task78/Solution.java
@@ -1,12 +1,29 @@
 package leetcode.task78;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @see <a href="https://leetcode-cn.com/problems/subsets/">link</a>
  */
 public class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> current = new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        return null;
+        dfs(nums, 0);
+        return result;
+    }
+
+    void dfs(int[] nums, int i) {
+        if (i == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        dfs(nums, i + 1);
+        current.add(nums[i]);
+        dfs(nums, i + 1);
+        current.remove(current.size() - 1);
     }
 }
