a9f373309d57feadaf6fb94f1709b75948df3437
syf <2654525303@qq.com>
2021-01-30 14:32:45 +0800

    finish task 77

--- leetcode/task77/Solution.java
+++ leetcode/task77/Solution.java
@@ -1,13 +1,36 @@
 package leetcode.task77;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @see <a href=https://leetcode-cn.com/problems/combinations/">link</a>
  */
 public class Solution {
-    public List<List<Integer>> combine(int n, int k) {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> current = new ArrayList<>();
+    int n;
+    int k;
 
-        return null;
+    public List<List<Integer>> combine(int n, int k) {
+        this.n = n;
+        this.k = k;
+
+        backTrack(1);
+
+        return result;
+    }
+
+    void backTrack(int i) {
+        if (current.size() == k) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int j = i; j <= n; j++) {
+            current.add(j);
+            backTrack(j + 1);
+            current.remove(current.size() - 1);
+        }
     }
 }
