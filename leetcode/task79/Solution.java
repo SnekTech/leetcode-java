5f5ee2159889360e7af0695b7375875a520d629f
syf <2654525303@qq.com>
2021-01-30 15:32:35 +0800

    set up task 79

---
+++ leetcode/task79/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.task79;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/word-search/">link</a>
+ */
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        return false;
+    }
+}
---
+++ leetcode/task79/Test1.java
@@ -0,0 +1,49 @@
+package leetcode.task79;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1 {
+    @Test
+    public void example1() {
+        char[][] board = new char[][] {
+                { 'A','B','C','E' },
+                { 'S','F','C','S' },
+                { 'A','D','E','E' }
+        };
+        String word = "ABCCED";
+
+        Solution solution = new Solution();
+        boolean actual = solution.exist(board, word);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void example2() {
+        char[][] board = new char[][] {
+                { 'A','B','C','E' },
+                { 'S','F','C','S' },
+                { 'A','D','E','E' }
+        };
+        String word = "SEE";
+
+        Solution solution = new Solution();
+        boolean actual = solution.exist(board, word);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void example3() {
+        char[][] board = new char[][] {
+                { 'A','B','C','E' },
+                { 'S','F','C','S' },
+                { 'A','D','E','E' }
+        };
+        String word = "ABCB";
+
+        Solution solution = new Solution();
+        boolean actual = solution.exist(board, word);
+        assertFalse(actual);
+    }
+}
