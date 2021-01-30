436902f77c9bcaf2f93704adade55a26439e8588
syf <2654525303@qq.com>
2021-01-30 20:39:31 +0800

    finish task 80

--- leetcode/task80/Solution.java
+++ leetcode/task80/Solution.java
@@ -5,6 +5,58 @@ package leetcode.task80;
  */
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int n = nums.length;
+
+        int sameCount = 1, duplicateCount = 0, current = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == current) {
+                sameCount++;
+                if (sameCount > 2) {
+                    nums[i] = Integer.MAX_VALUE;
+                    duplicateCount++;
+                }
+            }
+            else {
+                sameCount = 1;
+                current = nums[i];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < Integer.MAX_VALUE) {
+                continue;
+            }
+
+            int nextValidIndex = -1;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] < Integer.MAX_VALUE) {
+                    nextValidIndex = j;
+                    break;
+                }
+            }
+
+            if (nextValidIndex != -1) {
+                swap(nums, i, nextValidIndex);
+            }
+            else {
+                break;
+            }
+        }
+
+        return n - duplicateCount;
+    }
+
+    void swap(int[] nums, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
