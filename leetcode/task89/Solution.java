e38cb4c06f927000e1e326bd30b76cb9bb2a5511
syf <2654525303@qq.com>
2021-01-31 14:53:15 +0800

    finish task 89

--- leetcode/task89/Solution.java
+++ leetcode/task89/Solution.java
@@ -1,12 +1,50 @@
 package leetcode.task89;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @see <a href="https://leetcode-cn.com/problems/gray-code/">link</a>
  */
 public class Solution {
+    List<Integer> result = new ArrayList<>();
+    Set<Integer> gray = new HashSet<>();
+    int maxLength;
+    int n;
     public List<Integer> grayCode(int n) {
-        return null;
+        result.add(0);
+        gray.add(0);
+        if (n == 0) {
+            return result;
+        }
+        this.n = n;
+        maxLength = (int)Math.pow(2, n);
+
+        backTrack();
+
+        return result;
+    }
+
+    boolean backTrack() {
+        if (gray.size() == maxLength) {
+            return true;
+        }
+
+        int current = result.get(result.size() - 1);
+        for (int i = 0; i < n; i++) {
+            int next = current;
+            next ^= (1 << i);
+            if (gray.contains(next)) {
+                continue;
+            }
+            result.add(next);
+            gray.add(next);
+            if (backTrack()) {
+                return true;
+            }
+            result.remove(result.size() - 1);
+            gray.remove(next);
+        }
+
+        return false;
     }
 }
---
+++ leetcode/task89/Test1.java
@@ -0,0 +1,20 @@
+package leetcode.task89;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class Test1 {
+    @Test
+    public void example() {
+        int n = 2;
+    
+        Solution solution = new Solution();
+        int[] expected = new int[] { 0, 1, 3, 2 };
+        var actual = solution.grayCode(n);
+        assertTrue(true);
+    }
+}
