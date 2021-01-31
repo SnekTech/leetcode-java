df6c0f2696cf5b54931a2a3902fc3697ba57d3bc
syf <2654525303@qq.com>
2021-01-31 16:34:05 +0800

    try to solve task 91

--- leetcode/task91/Solution.java
+++ leetcode/task91/Solution.java
@@ -1,10 +1,48 @@
 package leetcode.task91;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/decode-ways/">link</a>
  */
 public class Solution {
+    Map<String, String> table = new HashMap<>();
+    Set<String> result = new HashSet<>();
+    StringBuilder decoded = new StringBuilder();
+
     public int numDecodings(String s) {
-        return 0;
+        for (int i = 0; i < 26; i++) {
+            table.put(String.valueOf(i + 1), Character.toString('A' + (char)i));
+        }
+
+        backTrack(s, 0);
+
+        return result.size();
+    }
+
+    void backTrack(String s, int index) {
+        if (index == s.length()) {
+            result.add(decoded.toString());
+            return;
+        }
+
+        String key = s.substring(index, index + 1);
+        if (table.containsKey(key)) {
+            decoded.append(table.get(key));
+            backTrack(s, index + 1);
+            decoded.deleteCharAt(decoded.length() - 1);
+        }
+
+        if (index < s.length() - 1) {
+            key = s.substring(index, index + 2);
+            if (table.containsKey(key)) {
+                decoded.append(table.get(key));
+                backTrack(s, index + 2);
+                decoded.deleteCharAt(decoded.length() - 1);
+            }
+        }
     }
 }
