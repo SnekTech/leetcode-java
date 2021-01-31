bfb2eba9cdb46f9164f9d8e24af1fdd624825576
syf <2654525303@qq.com>
2021-01-31 17:36:59 +0800

    finish task 93

--- leetcode/task93/Solution.java
+++ leetcode/task93/Solution.java
@@ -1,12 +1,56 @@
 package leetcode.task93;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @see <a href="https://leetcode-cn.com/problems/restore-ip-addresses/">link</a>
  */
 public class Solution {
+    static final int SEG_COUNT = 4;
+    List<String> ans = new ArrayList<>();
+    int[] segments = new int[SEG_COUNT];
+
     public List<String> restoreIpAddresses(String s) {
-        return null;
+        segments = new int[SEG_COUNT];
+        dfs(s, 0, 0);
+        return ans;
+    }
+
+    public void dfs(String s, int segId, int segStart) {
+        if (segId == SEG_COUNT) {
+            if (segStart == s.length()) {
+                StringBuilder ipAddr = new StringBuilder();
+                for (int i = 0; i < SEG_COUNT; i++) {
+                    ipAddr.append(segments[i]);
+                    if (i != SEG_COUNT - 1) {
+                        ipAddr.append('.');
+                    }
+                }
+                ans.add(ipAddr.toString());
+            }
+            return;
+        }
+
+        if (segStart == s.length()) {
+            return;
+        }
+
+        if (s.charAt(segStart) == '0') {
+            segments[segId] = 0;
+            dfs(s, segId + 1, segStart + 1);
+        }
+
+        int addr = 0;
+        for (int segEnd = segStart; segEnd < s.length(); segEnd++) {
+            addr = addr * 10 + (s.charAt(segEnd) - '0');
+            if (addr > 0 && addr <= 0xff) {
+                segments[segId] = addr;
+                dfs(s, segId + 1, segEnd + 1);
+            }
+            else {
+                break;
+            }
+        }
     }
 }
