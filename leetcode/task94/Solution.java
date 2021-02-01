1723b93cc44fcbd40ca127d314e94d42525db172
syf <2654525303@qq.com>
2021-02-01 14:41:53 +0800

    set up task 94

---
+++ leetcode/task94/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.task94;
+
+import leetcode.core.TreeNode;
+
+import java.util.List;
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        return null;
+    }
+}
