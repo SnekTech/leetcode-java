0093f812130671517d210894bbdcc34a143e5f14
syf <2654525303@qq.com>
2021-02-01 16:37:37 +0800

    set up task 98

---
+++ leetcode/task98/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.task98;
+
+import leetcode.core.TreeNode;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/validate-binary-search-tree/">link</a>
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return false;
+    }
+}
