77e04d48fbe8a0fa8cb5b25cdc5cdef9439c045c
syf <2654525303@qq.com>
2021-02-01 14:46:06 +0800

    set up task 95

---
+++ leetcode/task95/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.task95;
+
+import leetcode.core.TreeNode;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/unique-binary-search-trees-ii/">link</a>
+ */
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return null;
+    }
+}
