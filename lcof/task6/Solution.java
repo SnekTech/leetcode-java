117618ca3c8e609b89b066cecc8ac3c319a0f019
syf <2654525303@qq.com>
2021-02-06 19:06:30 +0800

    set up lcof 6

---
+++ lcof/task6/Solution.java
@@ -0,0 +1,12 @@
+package lcof.task6;
+
+import leetcode.core.ListNode;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/">link</a>
+ */
+public class Solution {
+    public int[] reversePrint(ListNode head) {
+        return null;
+    }
+}
