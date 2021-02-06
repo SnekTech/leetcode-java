f5d83bf1bb6c85735a25668a692c73e75651d86b
syf <2654525303@qq.com>
2021-02-06 19:26:03 +0800

    set up lcof 7

---
+++ lcof/task7/Solution.java
@@ -0,0 +1,12 @@
+package lcof.task7;
+
+import leetcode.core.TreeNode;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof/">link</a>
+ */
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return null;
+    }
+}
