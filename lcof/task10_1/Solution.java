c16f5034a29728e23b7a18bdcce8cad227ceb666
syf <2654525303@qq.com>
2021-02-07 01:10:18 +0800

    optimize lcof 10-1

--- lcof/task10_1/Solution.java
+++ lcof/task10_1/Solution.java
@@ -5,28 +5,14 @@ package lcof.task10_1;
  */
 public class Solution {
     public int fib(int n) {
-        int[] f = new int[101];
+        int a = 0, b = 1, sum;
 
-        int result = -1;
-
-        for (int i = 0; i <= n; i++) {
-            if (i == 0) {
-                f[i] = 0;
-            }
-            else if (i == 1) {
-                f[1] = 1;
-            }
-            else {
-                f[i] = (f[i - 2] + f[i - 1]) % 1000000007;
-            }
-
-            if (i == n) {
-                result = f[i];
-                break;
-            }
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
         }
 
-
-        return result;
+        return a;
     }
 }
