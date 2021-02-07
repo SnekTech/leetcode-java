0f0ab49106348b92c3e289923691f341863232ce
syf <2654525303@qq.com>
2021-02-07 16:32:24 +0800

    optimize lcof 15

--- README.md
+++ README.md
@@ -101,7 +101,7 @@ LeetCode(CN) solutions written in Java11.
 - [x] [13](lcof/task13/Solution.java)
 - [x] [14-1](lcof/task14_1/Solution.java)
 - [x] [14-2](lcof/task14_2/Solution.java)
-- [x] [15](lcof/task15/Solution.java) tbo
+- [x] [15](lcof/task15/Solution.java)
 - [x] [57-2](lcof/task57_2/Solution.java)
 
 ## Awesome Tools
--- lcof/task15/Solution.java
+++ lcof/task15/Solution.java
@@ -8,11 +8,9 @@ public class Solution {
     public int hammingWeight(int n) {
         int count = 0;
 
-        for (int i = 0; i < 32; i++) {
-            if ((n & 1) != 0) {
-                count++;
-            }
-            n >>= 1;
+        while (n != 0) {
+            count++;
+            n &= n - 1;
         }
 
         return count;
