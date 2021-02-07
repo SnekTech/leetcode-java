a5797019081a4be37b12b58a4bb9c754f62fafbf
syf <2654525303@qq.com>
2021-02-07 15:54:00 +0800

    optimize lcof 14-1

--- lcof/task14_1/Solution.java
+++ lcof/task14_1/Solution.java
@@ -4,28 +4,14 @@ package lcof.task14_1;
  * @see <a href="https://leetcode-cn.com/problems/jian-sheng-zi-lcof/">link</a>
  */
 class Solution {
-    int maxProduct = 1;
-
     public int cuttingRope(int n) {
-        backTrack(n, 1, 0, 0);
-
-        return maxProduct;
-    }
-
-    void backTrack(int n, int product, int length, int count) {
-        if (length > n) {
-            return;
-        }
-
-        if (length == n) {
-            if (count > 1) {
-                maxProduct = Math.max(product, maxProduct);
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
             }
-            return;
         }
 
-        for (int i = 1; i <= n - length; i++) {
-            backTrack(n, product * i, length + i, count + 1);
-        }
+        return dp[n];
     }
 }
