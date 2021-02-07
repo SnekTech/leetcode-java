050b06ecb9186a5a09461bd4204420c97b3ef740
syf <2654525303@qq.com>
2021-02-07 16:24:44 +0800

    finish lcof 15

--- lcof/task15/Solution.java
+++ lcof/task15/Solution.java
@@ -6,6 +6,15 @@ package lcof.task15;
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        return 0;
+        int count = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n >>= 1;
+        }
+
+        return count;
     }
 }
