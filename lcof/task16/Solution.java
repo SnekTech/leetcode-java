80c23c8760165a33c94907d98cfa3d7e00acea93
syf <2654525303@qq.com>
2021-02-08 15:07:27 +0800

    finish lcof 16

--- lcof/task16/Solution.java
+++ lcof/task16/Solution.java
@@ -3,8 +3,27 @@ package lcof.task16;
 /**
  * @see <a href="https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof/">link</a>
  */
+@SuppressWarnings("DuplicatedCode")
 class Solution {
+    @SuppressWarnings("UnnecessaryLocalVariable")
     public double myPow(double x, int n) {
-        return 0;
+        long N = n;
+        return N >= 0 ? quickPow(x, N) : 1.0 / quickPow(x, -N);
+    }
+
+    private double quickPow(double x, long N) {
+        double ans = 1.0;
+        double x_contribute = x;
+
+        while (N > 0) {
+            if ((N & 1) == 1) {
+                ans *= x_contribute;
+            }
+
+            x_contribute *= x_contribute;
+            N >>= 1;
+        }
+
+        return ans;
     }
 }
