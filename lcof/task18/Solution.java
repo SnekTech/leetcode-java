dc2d202cf97ca9af48c25c9d716ae0ce7fe7b87c
syf <2654525303@qq.com>
2021-02-08 15:16:50 +0800

    prepare lcof 18

---
+++ lcof/task18/Solution.java
@@ -0,0 +1,12 @@
+package lcof.task18;
+
+import leetcode.core.ListNode;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/">link</a>
+ */
+class Solution {
+    public ListNode deleteNode(ListNode head, int val) {
+        return null;
+    }
+}
