83eba4138a964ce5d554b8f8befa41e54ad87a45
syf <2654525303@qq.com>
2021-02-09 16:00:42 +0800

    prepare lcof 26

---
+++ lcof/task26/Solution.java
@@ -0,0 +1,12 @@
+package lcof.task26;
+
+import leetcode.core.TreeNode;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof/">link</a>
+ */
+class Solution {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return false;
+    }
+}
