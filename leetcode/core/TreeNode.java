57b7474e16009fa95866a96aa23bd752b9d9ccac
syf <2654525303@qq.com>
2021-02-09 16:09:53 +0800

    add binary tree test function

--- leetcode/core/TreeNode.java
+++ leetcode/core/TreeNode.java
@@ -13,4 +13,16 @@ public class TreeNode {
         this.left = left;
         this.right = right;
     }
+
+    public static boolean AreTreesEqual(TreeNode tree1, TreeNode tree2) {
+        if (tree1 == null && tree2 == null) {
+            return true;
+        }
+
+        if (tree1 == null || tree2 == null) {
+            return false;
+        }
+
+        return tree1.val == tree2.val && AreTreesEqual(tree1.left, tree2.left) && AreTreesEqual(tree1.right, tree2.right);
+    }
 }
