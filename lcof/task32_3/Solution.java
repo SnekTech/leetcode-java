950d4156707a7c684c388c30b92ca7dceec1fb2a
syf <2654525303@qq.com>
2021-02-11 21:31:39 +0800

    prepare lcof 32-3

---
+++ lcof/task32_3/Solution.java
@@ -0,0 +1,14 @@
+package lcof.task32_3;
+
+import leetcode.core.TreeNode;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/">link</a>
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+}
