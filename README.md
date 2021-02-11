b22e605809caddc9d87738df5ae0a10ca93a0e76
syf <2654525303@qq.com>
2021-02-11 16:02:32 +0800

    finish lcof 31

--- README.md
+++ README.md
@@ -116,7 +116,7 @@ LeetCode(CN) solutions written in Java11.
 - [x] [28](lcof/task28/Solution.java)
 - [x] [29](lcof/task29/Solution.java)
 - [x] [30](lcof/task30/MinStack.java)
-- [ ] [31](lcof/task31/Solution.java)
+- [x] [31](lcof/task31/Solution.java) tbo
 - [x] [57-2](lcof/task57_2/Solution.java)
 
 ## Awesome Tools
--- lcof/task31/Solution.java
+++ lcof/task31/Solution.java
@@ -1,10 +1,44 @@
 package lcof.task31;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/zhan-de-ya-ru-dan-chu-xu-lie-lcof/">link</a>
  */
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        return false;
+        int n = pushed.length;
+        if (n == 0) {
+            return true;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(pushed[i], i);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int prevIndex = -1;
+        for (int i = 0; i < n; i++) {
+            int index = map.get(popped[i]);
+            if (index > prevIndex) {
+                for (int j = prevIndex + 1; j < index; j++) {
+                    stack.push(pushed[j]);
+                }
+                prevIndex = index;
+            }
+            else {
+                if (stack.peek() == popped[i]) {
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
     }
 }
