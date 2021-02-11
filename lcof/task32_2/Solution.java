34e1682cabdec779b7f7dad5d4388c496284688a
syf <2654525303@qq.com>
2021-02-11 17:09:59 +0800

    finish lcof 32-2

--- lcof/task32_2/Solution.java
+++ lcof/task32_2/Solution.java
@@ -2,14 +2,54 @@ package lcof.task32_2;
 
 import leetcode.core.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @see <a href="https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/">link</a>
  */
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    static class NodeWithLevel {
+        TreeNode node;
+        int level;
 
-        return null;
+        public NodeWithLevel(TreeNode node, int level) {
+            this.node = node;
+            this.level = level;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<NodeWithLevel> queue = new LinkedList<>();
+        queue.add(new NodeWithLevel(root, 1));
+        int prevLevel = 0;
+        List<Integer> list = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            var current = queue.poll();
+            var node = current.node;
+            if (current.level != prevLevel) {
+                prevLevel = current.level;
+                list = new ArrayList<>();
+                result.add(list);
+            }
+            list.add(node.val);
+
+            if (node.left != null) {
+                queue.add(new NodeWithLevel(node.left, current.level + 1));
+            }
+            if (node.right != null) {
+                queue.add(new NodeWithLevel(node.right, current.level + 1));
+            }
+        }
+
+        return result;
     }
 }
---
+++ lcof/task32_2/Test1.java
@@ -0,0 +1,21 @@
+package lcof.task32_2;
+
+import leetcode.core.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1 {
+    @Test
+    public void example1() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Solution solution = new Solution();
+        var actual = solution.levelOrder(root);
+        assertTrue(true);
+    }
+}
