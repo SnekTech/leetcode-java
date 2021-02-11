d9559c8dc6cebd7ff11a2aa6535c8ed3d53b9f7a
syf <2654525303@qq.com>
2021-02-11 16:07:52 +0800

    optimize lcof 31

--- lcof/task31/Solution.java
+++ lcof/task31/Solution.java
@@ -1,7 +1,5 @@
 package lcof.task31;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -9,36 +7,16 @@ import java.util.Stack;
  */
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        int n = pushed.length;
-        if (n == 0) {
-            return true;
-        }
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(pushed[i], i);
-        }
-
         Stack<Integer> stack = new Stack<>();
-        int prevIndex = -1;
-        for (int i = 0; i < n; i++) {
-            int index = map.get(popped[i]);
-            if (index > prevIndex) {
-                for (int j = prevIndex + 1; j < index; j++) {
-                    stack.push(pushed[j]);
-                }
-                prevIndex = index;
+        int i = 0;
+        for (int num : pushed) {
+            stack.push(num);
+            while (!stack.isEmpty() && stack.peek() == popped[i]) {
+                stack.pop();
+                i++;
             }
-            else {
-                if (stack.peek() == popped[i]) {
-                    stack.pop();
-                }
-                else {
-                    return false;
-                }
-            }
-
         }
 
-        return true;
+        return stack.isEmpty();
     }
 }
