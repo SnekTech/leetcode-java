0b25a138f724a4330575ea3d3c589b39ebf78ad8
syf <2654525303@qq.com>
2021-02-12 13:46:27 +0800

    prepare lcof 34

---
+++ lcof/task34/Solution.java
@@ -0,0 +1,14 @@
+package lcof.task34;
+
+import leetcode.core.TreeNode;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/">link</a>
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        return null;
+    }
+}
