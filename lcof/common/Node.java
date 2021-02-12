bf5a252cb5ac1104d508782d6744fab42c692fac
syf <2654525303@qq.com>
2021-02-12 15:05:11 +0800

    prepare lcof 35

---
+++ lcof/common/Node.java
@@ -0,0 +1,13 @@
+package lcof.common;
+
+public class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int val) {
+        this.val = val;
+        next = null;
+        random = null;
+    }
+}
---
+++ lcof/task35/Solution.java
@@ -0,0 +1,12 @@
+package lcof.task35;
+
+import lcof.common.Node;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof/">link</a>
+ */
+class Solution {
+    public Node copyRandomList(Node head) {
+        return null;
+    }
+}
