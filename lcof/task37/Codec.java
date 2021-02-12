cd12dfb6672a2e15dfbea329caf1b456c3b0cad4
syf <2654525303@qq.com>
2021-02-12 18:44:06 +0800

    fail to solve lcof 37

--- lcof/task37/Codec.java
+++ lcof/task37/Codec.java
@@ -2,6 +2,10 @@ package lcof.task37;
 
 import leetcode.core.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/xu-lie-hua-er-cha-shu-lcof/">link</a>
  */
@@ -9,11 +13,89 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        return null;
+        StringBuilder str = new StringBuilder();
+        str.append('[');
+        if (root == null) {
+            str.append(']');
+            return str.toString();
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        LinkedList<TreeNode> list = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            var node = queue.remove();
+            list.addLast(node);
+
+            if (node != null) {
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+
+        while (list.peekLast() == null) {
+            list.removeLast();
+        }
+
+        while (!list.isEmpty()) {
+            var node = list.removeFirst();
+            if (node == null) {
+                str.append("null");
+            }
+            else {
+                str.append(node.val);
+            }
+
+            if (!list.isEmpty()) {
+                str.append(',');
+            }
+        }
+
+        str.append(']');
+
+        return str.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        return null;
+        if (data.equals("[]")) {
+            return null;
+        }
+        data = data.substring(1, data.length() - 1);
+        var raw = data.split(",");
+        int n = raw.length;
+
+        // 此处前后各多加一个元素 null
+        // 前者为了调整索引值，方便计算二叉树左右孩子索引
+        // 后者为了在计算最后一个非叶节点的右孩子时防止数组越界
+        ArrayList<TreeNode> treeNodes = new ArrayList<>(n * 2);
+        for (int i = 0; i < 2 * n + 2; i++) {
+            treeNodes.add(null);
+        }
+        for (int i = 1; i <= n; i++) {
+            var t = raw[i - 1];
+            if (!t.equals("null")) {
+                treeNodes.set(i, new TreeNode(Integer.parseInt(t)));
+            }
+        }
+
+        for (int i = 1; i <= (int)Math.pow(2, (double)n / 2); i++) {
+            if (treeNodes.get(i) == null) {
+                treeNodes.add(2 * i, null);
+                treeNodes.add(2 * i + 1, null);
+            }
+        }
+
+        for (int i = 1; i <= treeNodes.size() / 2; i++) {
+            var node = treeNodes.get(i);
+            if (node == null) {
+                continue;
+            }
+
+            node.left = treeNodes.get(2 * i);
+            node.right = treeNodes.get(2 * i + 1);
+        }
+
+        return treeNodes.get(1);
     }
 }
--- lcof/task37/Test1.java
+++ lcof/task37/Test1.java
@@ -33,4 +33,65 @@ public class Test1 {
         var actual = codec.deserialize(data);
         assertTrue(TreeNode.areTreesEqual(expected, actual));
     }
+
+    @Test
+    public void wrong1() {
+        String data = "[1,2]";
+
+        Codec codec = new Codec();
+        TreeNode expected = new TreeNode(1);
+        expected.left = new TreeNode(2);
+        var actual = codec.deserialize(data);
+        assertTrue(TreeNode.areTreesEqual(expected, actual));
+    }
+
+    @Test
+    public void wrong2() {
+        String data = "[5,2,3,null,null,2,4,3,1]";
+
+        Codec codec = new Codec();
+        TreeNode expected = new TreeNode(5);
+        expected.left = new TreeNode(2);
+        expected.right = new TreeNode(3);
+        expected.right.left = new TreeNode(2);
+        expected.right.right = new TreeNode(4);
+        expected.right.left.left = new TreeNode(3);
+        expected.right.left.right = new TreeNode(1);
+
+        var actual = codec.deserialize(data);
+        assertTrue(TreeNode.areTreesEqual(expected, actual));
+    }
+
+    @Test
+    public void wrong3() {
+        String data = "[2,null,3,null,4,null,5,null,6]";
+
+        Codec codec = new Codec();
+        TreeNode expected = new TreeNode(2);
+        expected.right = new TreeNode(3);
+        expected.right.right = new TreeNode(4);
+        expected.right.right.right = new TreeNode(5);
+        expected.right.right.right.right = new TreeNode(6);
+
+        var actual = codec.deserialize(data);
+        assertTrue(TreeNode.areTreesEqual(expected, actual));
+    }
+
+    @Test
+    public void wrong4() {
+        String data = "[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null]";
+
+        Codec codec = new Codec();
+        TreeNode expected = new TreeNode(1);
+        expected.right = new TreeNode(2);
+        expected.right.right = new TreeNode(3);
+        expected.right.right.right = new TreeNode(4);
+        expected.right.right.right.right = new TreeNode(5);
+        expected.right.right.right.right.right = new TreeNode(6);
+        expected.right.right.right.right.right.right = new TreeNode(7);
+        expected.right.right.right.right.right.right.right = new TreeNode(8);
+
+        var actual = codec.deserialize(data);
+        assertTrue(TreeNode.areTreesEqual(expected, actual));
+    }
 }
