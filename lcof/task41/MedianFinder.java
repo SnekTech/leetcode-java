74bb3d41d5443f7d5ca3dcc17c423f504f351cfe
syf <2654525303@qq.com>
2021-02-13 20:04:57 +0800

    finish lcof 41

--- lcof/task41/MedianFinder.java
+++ lcof/task41/MedianFinder.java
@@ -1,17 +1,62 @@
 package lcof.task41;
 
+import java.util.ArrayList;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/shu-ju-liu-zhong-de-zhong-wei-shu-lcof/">link</a>
+ */
 class MedianFinder {
+    ArrayList<Integer> data;
 
     /** initialize your data structure here. */
     public MedianFinder() {
-
+        data = new ArrayList<>(50000);
     }
 
     public void addNum(int num) {
+        int pos = 0;
+        int left = 0, right = data.size() - 1;
+        while (left <= right) {
+            if (right - left == 1) {
+                if (num <= data.get(left)) {
+                    pos = left;
+                }
+                else if (num <= data.get(right)) {
+                    pos = right;
+                }
+                else {
+                    pos = right + 1;
+                }
+                break;
+            }
 
+            if (right == left) {
+                pos = num <= data.get(left) ? left : left + 1;
+                break;
+            }
+
+            int mid = left + (right - left) / 2;
+            if (num == data.get(mid)) {
+                pos = mid;
+                break;
+            }
+            else if (num < data.get(mid)) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        data.add(pos, num);
     }
 
     public double findMedian() {
-        return 0;
+        int size = data.size();
+        if (size == 0) {
+            return 0;
+        }
+
+        return size % 2 == 0 ? (data.get(size / 2 - 1) + data.get(size / 2)) / 2.0 : data.get(size / 2);
     }
 }
--- lcof/task41/Test1.java
+++ lcof/task41/Test1.java
@@ -18,4 +18,37 @@ public class Test1 {
 
         assertTrue(true);
     }
+
+    @Test
+    public void wrong1() {
+
+        MedianFinder medianFinder = new MedianFinder();
+
+        medianFinder.addNum(6);
+        medianFinder.addNum(10);
+        var x1 = medianFinder.findMedian();
+        medianFinder.addNum(2);
+        var x2 = medianFinder.findMedian();
+
+        assertTrue(true);
+    }
+
+    @Test
+    public void wrong2() {
+
+        MedianFinder medianFinder = new MedianFinder();
+
+        medianFinder.addNum(-1);
+        var x1 = medianFinder.findMedian();
+        medianFinder.addNum(-2);
+        var x2 = medianFinder.findMedian();
+        medianFinder.addNum(-3);
+        var x3 = medianFinder.findMedian();
+        medianFinder.addNum(-4);
+        var x4 = medianFinder.findMedian();
+        medianFinder.addNum(-5);
+        var x5 = medianFinder.findMedian();
+
+        assertTrue(true);
+    }
 }
