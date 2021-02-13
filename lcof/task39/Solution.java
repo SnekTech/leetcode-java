e6376a13a9c074a42f2c6addef9a426d89d1808f
syf <2654525303@qq.com>
2021-02-13 16:04:43 +0800

    optimize lcof 39

--- lcof/task39/Solution.java
+++ lcof/task39/Solution.java
@@ -1,28 +1,23 @@
 package lcof.task39;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @see <a href="https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/">link</a>
  */
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int major = -1;
+        int vote = 0;
+
         for (var num : nums) {
-            if (map.containsKey(num)) {
-                map.put(num, map.get(num) + 1);
+            if (vote == 0) {
+                major = num;
+            }
+
+            if (major == num) {
+                vote++;
             }
             else {
-                map.put(num, 1);
-            }
-        }
-
-        int major = -1;
-        int minCount = nums.length % 2 == 0 ? nums.length / 2 : nums.length / 2 + 1;
-        for (var entry : map.entrySet()) {
-            if (entry.getValue() >= minCount) {
-                major = entry.getKey();
+                vote--;
             }
         }
 
