d2146ece0c950e9311087249a657ec6f4af2229b
syf <2654525303@qq.com>
2021-02-14 17:40:22 +0800

    finish lcof 47

--- lcof/task47/Solution.java
+++ lcof/task47/Solution.java
@@ -5,6 +5,40 @@ package lcof.task47;
  */
 class Solution {
     public int maxValue(int[][] grid) {
-        return 0;
+        if (grid == null) {
+            return 0;
+        }
+        int m = grid.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = grid[0].length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                    continue;
+                }
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                    continue;
+                }
+
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 }
