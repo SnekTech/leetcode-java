95314153169d4724f3898762cd4ca7a65be987e3
syf <2654525303@qq.com>
2021-02-14 14:56:55 +0800

    finish(copy) lcof 44

--- lcof/task44/Solution.java
+++ lcof/task44/Solution.java
@@ -5,6 +5,19 @@ package lcof.task44;
  */
 class Solution {
     public int findNthDigit(int n) {
-        return 0;
+        // 抄的
+        int digit = 1;
+        long start = 1;
+        long count = 9;
+
+        while (n > count) {
+            n -= count;
+            digit++;
+            start *= 10;
+            count = digit * start * 9;
+        }
+        long num = start + (n - 1) / digit;
+
+        return Long.toString(num).charAt((n - 1) % digit) - '0';
     }
 }
