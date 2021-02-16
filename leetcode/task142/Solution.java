494db648778ba8bff6d5c5e44477415f0525690b
syf <2654525303@qq.com>
2021-02-16 16:34:32 +0800

    finish leetcode 142

--- leetcode/task142/Solution.java
+++ leetcode/task142/Solution.java
@@ -2,11 +2,24 @@ package leetcode.task142;
 
 import leetcode.core.ListNode;
 
+import java.util.HashSet;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/linked-list-cycle-ii/">link</a>
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+
+        var p = head;
+        while (p != null) {
+            set.add(p);
+            if (set.contains(p.next)) {
+                return p.next;
+            }
+            p = p.next;
+        }
+
         return null;
     }
 }
