7133b43f95695935edbea224c72ca1a72dc04390
syf <2654525303@qq.com>
2021-02-16 11:57:01 +0800

    finish leetcode 215

--- leetcode/task215/Solution.java
+++ leetcode/task215/Solution.java
@@ -1,10 +1,27 @@
 package leetcode.task215;
 
+import java.util.PriorityQueue;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/kth-largest-element-in-an-array/">link</a>
  */
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        return 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k); // min-root
+
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i < k) {
+                queue.offer(nums[i]);
+            }
+            else {
+                queue.offer(nums[i]);
+                queue.poll();
+            }
+        }
+
+        assert !queue.isEmpty();
+
+        return queue.peek();
     }
 }
