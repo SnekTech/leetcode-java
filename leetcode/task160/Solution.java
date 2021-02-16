ad111f0e993450bc6dd9fcb1eef8426d45353336
syf <2654525303@qq.com>
2021-02-16 15:28:10 +0800

    prepare leetcode 160

---
+++ leetcode/task160/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.task160;
+
+import leetcode.core.ListNode;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/intersection-of-two-linked-lists/">link</a>
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return null;
+    }
+}
---
+++ leetcode/task160/Test1.java
@@ -0,0 +1,20 @@
+package leetcode.task160;
+
+import leetcode.core.ListNode;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1 {
+    @Test
+    public void example1() {
+        ListNode h1 = ListNode.createList(new int[] {4,1,8,4,5});
+        ListNode h2 = ListNode.createList(new int[] {5, 0});
+        h2.next.next = h1;
+
+        Solution solution = new Solution();
+        var expected = h1.next;
+        var actual = solution.getIntersectionNode(h1, h2);
+        assertSame(expected, actual);
+    }
+}
