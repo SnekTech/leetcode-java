af075f15cfe7b3bcc83523037887b9155b8c25f0
syf <2654525303@qq.com>
2021-02-17 22:20:21 +0800

    finish leetcode 150

--- leetcode/task150/Solution.java
+++ leetcode/task150/Solution.java
@@ -1,10 +1,44 @@
 package leetcode.task150;
 
+import java.util.Stack;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/">link</a>
  */
 class Solution {
     public int evalRPN(String[] tokens) {
-        return 0;
+        int n = tokens.length;
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                var b = stack.pop();
+                var a = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(a + b);
+                        break;
+                    case "-":
+                        stack.push(a - b);
+                        break;
+                    case "*":
+                        stack.push(a * b);
+                        break;
+                    case "/":
+                        stack.push(a / b);
+                        break;
+                }
+            } else {
+                stack.push(Integer.valueOf(token));
+            }
+        }
+
+        assert stack.size() == 1;
+
+        return stack.peek();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
     }
 }
