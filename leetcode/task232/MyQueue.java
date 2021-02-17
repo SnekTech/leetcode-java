0cadcc4da76d8f71061d1fafe68cfd7002ec3173
syf <2654525303@qq.com>
2021-02-17 22:03:56 +0800

    finish leetcode 232

--- leetcode/task232/MyQueue.java
+++ leetcode/task232/MyQueue.java
@@ -1,32 +1,59 @@
 package leetcode.task232;
 
+import java.util.Stack;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/implement-queue-using-stacks/">link</a>
  */
 class MyQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
 
     /** Initialize your data structure here. */
     public MyQueue() {
-
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
     }
 
     /** Push element x to the back of queue. */
     public void push(int x) {
-
+        stack1.push(x);
     }
 
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        return 0;
+        if (!stack2.isEmpty()) {
+            return stack2.pop();
+        }
+
+        if (stack1.isEmpty()) {
+            throw new IllegalArgumentException("cannot pop from empty queue");
+        }
+
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        return stack2.pop();
     }
 
     /** Get the front element. */
     public int peek() {
-        return 0;
+        if (!stack2.isEmpty()) {
+            return stack2.peek();
+        }
+
+        if (stack1.isEmpty()) {
+            throw new IllegalArgumentException("cannot peek from empty queue");
+        }
+
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        return stack2.peek();
     }
 
     /** Returns whether the queue is empty. */
     public boolean empty() {
-        return false;
+        return stack1.empty() && stack2.empty();
     }
 }
