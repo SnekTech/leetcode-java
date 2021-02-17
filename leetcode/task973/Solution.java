0c3a22b203e7845130e94827c7b1803c0afa3c65
syf <2654525303@qq.com>
2021-02-17 11:37:49 +0800

    use primitive int instead of Integer

--- leetcode/task973/Solution.java
+++ leetcode/task973/Solution.java
@@ -7,7 +7,7 @@ import java.util.PriorityQueue;
  */
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Integer[]> queue = new PriorityQueue<>(
+        PriorityQueue<int[]> queue = new PriorityQueue<>(
                 (a, b) -> getDistance(b[0], b[1]) - getDistance(a[0], a[1])
         );
         int[][] result = new int[k][2];
@@ -16,22 +16,20 @@ class Solution {
         for (int i = 0; i < n; i++) {
             int x = points[i][0], y = points[i][1];
             if (i < k) {
-                queue.add(new Integer[] {x, y});
+                queue.add(new int[] {x, y});
             }
             else {
                 assert queue.peek() != null;
                 int minX = queue.peek()[0], minY = queue.peek()[1];
                 if (getDistance(x, y) < getDistance(minX, minY)) {
                     queue.poll();
-                    queue.add(new Integer[] {x, y});
+                    queue.add(new int[] {x, y});
                 }
             }
         }
 
         for (int i = 0; !queue.isEmpty(); i++) {
-            var point = queue.poll();
-            result[i][0] = point[0];
-            result[i][1] = point[1];
+            result[i] = queue.poll();
         }
 
         return result;
