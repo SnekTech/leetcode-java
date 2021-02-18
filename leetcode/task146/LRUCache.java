8947e4bd798a32a5136f936fd1158e5cbeac7d7b
syf <2654525303@qq.com>
2021-02-18 16:43:38 +0800

    finish leetcode 146

--- leetcode/task146/LRUCache.java
+++ leetcode/task146/LRUCache.java
@@ -1,19 +1,47 @@
 package leetcode.task146;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/lru-cache/">link</a>
  */
 class LRUCache {
+    int k;
+    HashMap<Integer, Integer> dataMap;
+    LinkedList<Integer> list = new LinkedList<>();
 
     public LRUCache(int capacity) {
-
+        k = capacity;
+        dataMap = new HashMap<>(k);
     }
 
     public int get(int key) {
+        if (dataMap.containsKey(key)) {
+            var value = dataMap.get(key);
+            list.removeFirstOccurrence(key);
+            list.addLast(key);
+            return value;
+        }
+
         return -1;
     }
 
     public void put(int key, int value) {
+        if (dataMap.containsKey(key)) {
+            dataMap.put(key, value);
+            list.remove(Integer.valueOf(key));
+            list.addLast(key);
+            return;
+        }
 
+        if (dataMap.size() >= k) {
+            var minKey = list.pollFirst();
+
+            dataMap.remove(minKey);
+
+        }
+        dataMap.put(key, value);
+        list.addLast(key);
     }
 }
--- leetcode/task146/Test1.java
+++ leetcode/task146/Test1.java
@@ -19,4 +19,17 @@ public class Test1 {
         var x5 = cache.get(4);
         assertTrue(true);
     }
+
+    @Test
+    public void wrong1() {
+        LRUCache cache = new LRUCache(2);
+        var x1 = cache.get(2);
+        cache.put(2, 6);
+        var x2 = cache.get(1);
+        cache.put(1, 5);
+        cache.put(1, 2);
+        var x3 = cache.get(1);
+        var x4 = cache.get(2);
+        assertTrue(true);
+    }
 }
