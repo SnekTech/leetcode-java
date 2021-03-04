b05d911cd2c9d049a9d8bb8101d1a9828038d58a
syf <2654525303@qq.com>
2021-03-04 20:44:19 +0800

    finish leetcode 102

--- leetcode/task102/Solution.java
+++ leetcode/task102/Solution.java
@@ -4,8 +4,38 @@ import leetcode.core.TreeNode;
 
 import java.util.*;
 
+/**
+ * @see <a href="https://leetcode-cn.com/problems/binary-tree-level-order-traversal/">link</a>
+ */
+@SuppressWarnings("DuplicatedCode")
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> line = new ArrayList<>();
+            int lineSize = queue.size();
+            for (int i = 0; i < lineSize; i++) {
+                var node = queue.remove();
+                // access
+                line.add(node.val);
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            result.add(line);
+        }
+
+        return result;
     }
 }
