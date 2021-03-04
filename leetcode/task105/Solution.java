9bd1a7f9a180fe4f19501f4296bb84f541267ea5
syf <2654525303@qq.com>
2021-03-04 21:16:12 +0800

    finish leetcode 105

--- leetcode/task105/Solution.java
+++ leetcode/task105/Solution.java
@@ -7,6 +7,38 @@ import leetcode.core.TreeNode;
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return null;
+        if (preorder == null || inorder == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int n1 = preorder.length, n2 = inorder.length;
+        if (n1 != n2) {
+            throw new IllegalArgumentException();
+        }
+
+        return build(preorder, inorder, 0, n1 -1, 0, n1 - 1);
+    }
+
+    TreeNode build(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd) {
+        if (preStart > preEnd || inStart > inEnd) {
+            return null;
+        }
+
+        int rootVal = preorder[preStart];
+        TreeNode root = new TreeNode(rootVal);
+        int rootIndex = -1;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == rootVal) {
+                rootIndex = i;
+                break;
+            }
+        }
+        assert rootIndex != -1;
+        int leftLength = rootIndex - inStart;
+        int rightLenth = inEnd - rootIndex;
+
+        root.left = build(preorder, inorder, preStart + 1, preStart + 1 + leftLength - 1, inStart, rootIndex - 1);
+        root.right = build(preorder, inorder, preEnd - rightLenth + 1, preEnd, rootIndex + 1, inEnd);
+        return root;
     }
 }
