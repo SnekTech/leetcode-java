3a2a39995275c9eef5c9e534f25df0c86baf4f41
syf <2654525303@qq.com>
2021-03-04 20:09:07 +0800

    prepare leetcode 101

---
+++ leetcode/task101/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.task101;
+
+import leetcode.core.TreeNode;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/symmetric-tree/">link</a>
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+}
