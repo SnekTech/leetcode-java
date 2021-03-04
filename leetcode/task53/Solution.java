fae0a9760042391f3b3fd07de3909495e145679a
syf <2654525303@qq.com>
2021-03-04 19:56:18 +0800

    finish leetcode 53

--- leetcode/task53/Solution.java
+++ leetcode/task53/Solution.java
@@ -5,6 +5,19 @@ package leetcode.task53;
  */
 class Solution {
     public int maxSubArray(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(dp[i], max);
+        }
+
+        return max;
     }
 }
