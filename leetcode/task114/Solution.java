ebd8a2b0a7ffa94e40c0b8741c60ca41da20d107
syf <2654525303@qq.com>
2021-03-04 21:51:56 +0800

    finish leetcode 114

--- leetcode/task114/Solution.java
+++ leetcode/task114/Solution.java
@@ -7,6 +7,22 @@ import leetcode.core.TreeNode;
  */
 class Solution {
     public void flatten(TreeNode root) {
+        flattenReturnTail(root);
+    }
 
+    TreeNode flattenReturnTail(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        var originRight = root.right;
+        var leftTail = flattenReturnTail(root.left);
+        if (leftTail != null) {
+            leftTail.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        var rightTail = flattenReturnTail(originRight);
+        return rightTail != null ? rightTail : leftTail != null ? leftTail : root;
     }
 }
