a1fa552183ee6c5a8667db4b04a7a0455e54f353
syf <2654525303@qq.com>
2021-03-04 20:05:37 +0800

    finish leetcode 70

--- leetcode/task70/Solution.java
+++ leetcode/task70/Solution.java
@@ -5,6 +5,17 @@ package leetcode.task70;
  */
 class Solution {
     public int climbStairs(int n) {
-        return 0;
+        if (n <= 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[n];
     }
 }
