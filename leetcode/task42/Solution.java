41828a7ae2ce7e9ef45a7c61b541786bfb01fa7a
syf <2654525303@qq.com>
2021-03-05 20:44:15 +0800

    finish leetcode 42

--- leetcode/task42/Solution.java
+++ leetcode/task42/Solution.java
@@ -5,6 +5,21 @@ package leetcode.task42;
  */
 class Solution {
     public int trap(int[] height) {
-        return 0;
+        int n = height.length;
+        int sum = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            int maxLeft = 0, maxRight = 0;
+            for (int j = i; j >= 0; j--) {
+                maxLeft = Math.max(maxLeft, height[j]);
+            }
+            for (int j = i; j < n; j++) {
+                maxRight = Math.max(maxRight, height[j]);
+            }
+
+            sum += Math.min(maxLeft, maxRight) - height[i];
+        }
+
+        return sum;
     }
 }
