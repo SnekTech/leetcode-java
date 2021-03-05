77c27db5065453e036091a9f07d86da5b739c79c
syf <2654525303@qq.com>
2021-03-05 15:27:44 +0800

    finish leetcode 128

--- leetcode/task128/Solution.java
+++ leetcode/task128/Solution.java
@@ -1,10 +1,33 @@
 package leetcode.task128;
 
+import java.util.Arrays;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/longest-consecutive-sequence/">link</a>
  */
 class Solution {
     public int longestConsecutive(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int n = nums.length;
+        int maxLength = 1, currentLength = 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i + 1] == nums[i]) {
+                continue;
+            }
+            else if (nums[i + 1] == nums[i] + 1) {
+                currentLength++;
+            }
+            else {
+                currentLength = 1;
+            }
+
+            maxLength = Math.max(maxLength, currentLength);
+        }
+
+        return maxLength;
     }
 }
