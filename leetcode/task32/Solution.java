e21b35e0574f0af0c16e6ab66b47c63aea757f81
syf <2654525303@qq.com>
2021-03-05 17:05:55 +0800

    prepare leetcode 32

---
+++ leetcode/task32/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.task32;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/longest-valid-parentheses/">link</a>
+ */
+class Solution {
+    public int longestValidParentheses(String s) {
+        return 0;
+    }
+}
---
+++ leetcode/task32/Test1.java
@@ -0,0 +1,37 @@
+package leetcode.task32;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1 {
+    @Test
+    public void example1() {
+        String s = "(()";
+
+        Solution solution = new Solution();
+        int expected = 2;
+        int actual = solution.longestValidParentheses(s);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void example2() {
+        String s = ")()())";
+
+        Solution solution = new Solution();
+        int expected = 4;
+        int actual = solution.longestValidParentheses(s);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void example3() {
+        String s = "";
+
+        Solution solution = new Solution();
+        int expected = 0;
+        int actual = solution.longestValidParentheses(s);
+        assertEquals(expected, actual);
+    }
+}
