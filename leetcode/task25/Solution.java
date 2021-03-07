0ce4a665acef5314a7cc2e4839a9d5d14b9410ae
syf <2654525303@qq.com>
2021-03-07 19:21:16 +0800

    finish leetcode 25

--- leetcode/task25/Solution.java
+++ leetcode/task25/Solution.java
@@ -7,6 +7,38 @@ import leetcode.core.ListNode;
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        return null;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode left = head, right;
+
+        while (left != null) {
+            right = left;
+            boolean end = false;
+            for (int i = 0; i < k; i++) {
+                if (right == null) {
+                    end = true;
+                    break;
+                }
+                right = right.next;
+            }
+            if (end) {
+                break;
+            }
+
+            ListNode p = left;
+            for (int i = 0; i < k; i++) {
+                var nextNode = p.next;
+                p.next = prev.next;
+                prev.next = p;
+                p = nextNode;
+            }
+
+            left.next = right;
+            prev = left;
+            left = right;
+        }
+
+        return dummy.next;
     }
 }
