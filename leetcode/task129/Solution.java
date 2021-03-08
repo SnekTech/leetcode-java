b2cb4e42ace8e1a25e4aa6f6c27c783eeb493d77
syf <2654525303@qq.com>
2021-03-08 19:32:22 +0800

    finish leetcode 129

--- leetcode/task129/Solution.java
+++ leetcode/task129/Solution.java
@@ -7,6 +7,20 @@ import leetcode.core.TreeNode;
  */
 class Solution {
     public int sumNumbers(TreeNode root) {
-        return 0;
+        return dfs(root, 0);
+    }
+
+    int dfs(TreeNode root, int prevSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        int sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+        else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
     }
 }
