7c40be98df19e634e12061550bd4d6a9f4049838
syf <2654525303@qq.com>
2021-03-08 19:54:50 +0800

    prepare leetcode 208

---
+++ leetcode/task208/Trie.java
@@ -0,0 +1,34 @@
+package leetcode.task208;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/implement-trie-prefix-tree/">link</a>
+ */
+class Trie {
+
+    /** Initialize your data structure here. */
+    public Trie() {
+
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        return false;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Trie obj = new Trie();
+        obj.insert("apple");
+        boolean r1 = obj.search("apple");
+        boolean r2 = obj.startsWith("app");
+    }
+}
