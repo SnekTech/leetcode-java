6a597446ea3073c85b4ca6b6a93950788e38d427
syf <2654525303@qq.com>
2021-03-09 23:00:42 +0800

    finish leetcode 139

--- leetcode/task139/Solution.java
+++ leetcode/task139/Solution.java
@@ -7,6 +7,28 @@ import java.util.List;
  */
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return false;
+        int length = s.length();
+        boolean[] dp = new boolean[length];
+
+        for (int i = 0; i < length; i++) {
+            boolean isBreakable = false;
+            for (var word : wordDict) {
+                if (i - word.length() >= 0) {
+                    isBreakable = dp[i - word.length()] && s.substring(i - word.length() + 1, i + 1).equals(word);
+                    if (isBreakable) {
+                        break;
+                    }
+                }
+                else if (i - word.length() == -1) {
+                    isBreakable = s.substring(0, i + 1).equals(word);
+                    if (isBreakable) {
+                        break;
+                    }
+                }
+            }
+            dp[i] = isBreakable;
+        }
+
+        return dp[length - 1];
     }
 }
