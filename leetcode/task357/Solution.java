116ed689a13cf680e9d21a4c0bed8adebf02c0de
syf <2654525303@qq.com>
2021-03-14 15:25:04 +0800

    prepare leetcode 357

---
+++ leetcode/task357/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.task357;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/count-numbers-with-unique-digits/">link</a>
+ */
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        return 0;
+    }
+}
---
+++ leetcode/task357/Test1.java
@@ -0,0 +1,17 @@
+package leetcode.task357;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1 {
+    @Test
+    public void example1() {
+        int n = 2;
+        
+        Solution solution = new Solution();
+        int expected = 91;
+        int actual = solution.countNumbersWithUniqueDigits(n);
+        assertEquals(expected, actual);
+    }
+}
