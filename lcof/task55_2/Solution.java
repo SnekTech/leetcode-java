8028fa738d0e50c116e406989e612e6cff960958
syf <2654525303@qq.com>
2021-03-15 22:47:20 +0800

    finish lcof 55-2

--- lcof/task55_2/Solution.java
+++ lcof/task55_2/Solution.java
@@ -7,6 +7,18 @@ import leetcode.core.TreeNode;
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return false;
+        if (root == null) {
+            return true;
+        }
+
+        return isBalanced(root.left) && isBalanced(root.right) && Math.abs(height(root.left) - height(root.right)) <= 1;
+    }
+
+    int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(height(root.left), height(root.right)) + 1;
     }
 }
