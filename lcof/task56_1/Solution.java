f4cba32e4990bbdb44abacc5e861e2b3f8e36160
syf <2654525303@qq.com>
2021-03-15 23:01:31 +0800

    finish lcof 56-1

--- lcof/task56_1/Solution.java
+++ lcof/task56_1/Solution.java
@@ -1,10 +1,29 @@
 package lcof.task56_1;
 
+import java.util.HashSet;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/">link</a>
  */
 class Solution {
     public int[] singleNumbers(int[] nums) {
-        return null;
+        HashSet<Integer> set = new HashSet<>();
+        for (var x : nums) {
+            if (set.contains(x)) {
+                set.remove(x);
+            }
+            else {
+                set.add(x);
+            }
+        }
+
+        assert set.size() == 2;
+        int[] result = new int[2];
+        int i = 0;
+        for (var x : set) {
+            result[i++] = x;
+        }
+
+        return result;
     }
 }
