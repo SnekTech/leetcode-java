20b5cd5848ae337a6b78c2628ab5e79fd58eef55
syf <2654525303@qq.com>
2021-03-19 15:14:25 +0800

    finish lcof 64

--- lcof/task64/Solution.java
+++ lcof/task64/Solution.java
@@ -3,8 +3,10 @@ package lcof.task64;
 /**
  * @see <a href="https://leetcode-cn.com/problems/qiu-12n-lcof/">link</a>
  */
+@SuppressWarnings("unused")
 class Solution {
     public int sumNums(int n) {
-        return 0;
+        boolean x = n > 1 && (n += sumNums(n - 1)) > 0;
+        return n;
     }
 }
