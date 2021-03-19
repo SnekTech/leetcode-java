5a0a87b88009ade04f1a9d8abd4c302583f95b91
syf <2654525303@qq.com>
2021-03-19 12:54:13 +0800

    optimize lcof 60

--- lcof/task60/Solution.java
+++ lcof/task60/Solution.java
@@ -1,38 +1,25 @@
 package lcof.task60;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Arrays;
 
 /**
  * @see <a href="https://leetcode-cn.com/problems/nge-tou-zi-de-dian-shu-lcof/">link</a>
  */
 class Solution {
-    Map<Integer, Integer> map = new TreeMap<>();
     public double[] dicesProbability(int n) {
-        // 2 3 4 5 6 7 8 9 10 11 12
-        backTrack(n, 1, 0);
-        double total = 0;
-        for (var entry : map.entrySet()) {
-            total += entry.getValue();
-        }
-        double[] ans = new double[map.size()];
-        int i = 0;
-        for (var entry : map.entrySet()) {
-            ans[i] = entry.getValue() / total;
-            i++;
+        double[] dp = new double[6];
+        Arrays.fill(dp, 1 / 6.0);
+
+        for (int i = 2; i <= n; i++) {
+            double[] nextDP = new double[i * 5 + 1];
+            for (int j = 0; j < dp.length; j++) {
+                for (int k = 0; k < 6; k++) {
+                    nextDP[j + k] += dp[j] / 6.0;
+                }
+            }
+            dp = nextDP;
         }
 
-        return ans;
-    }
-
-    void backTrack(int n, int pos, int sum) {
-        if (pos > n) {
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
-            return;
-        }
-
-        for (int i = 1; i <= 6; i++) {
-            backTrack(n, pos + 1, sum + i);
-        }
+        return dp;
     }
 }
