08aed0ce722a5321901c4d26470badf1b1957a94
syf <2654525303@qq.com>
2021-03-19 20:47:42 +0800

    finish lcof 68-2

--- lcof/task68_2/Solution.java
+++ lcof/task68_2/Solution.java
@@ -7,6 +7,23 @@ import leetcode.core.TreeNode;
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return null;
+        if (root == null || p == null || q == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+
+        if (left != null) {
+            return left;
+        }
+        else return right;
     }
 }
