83eb2b351e34eb7c9c0674848d990d0b9514b954
syf <2654525303@qq.com>
2021-03-19 14:28:26 +0800

    finish lcof 62

--- lcof/task62/Solution.java
+++ lcof/task62/Solution.java
@@ -1,10 +1,24 @@
 package lcof.task62;
 
+import java.util.ArrayList;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/">link</a>
  */
 class Solution {
     public int lastRemaining(int n, int m) {
-        return 0;
+        ArrayList<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+
+        int index = 0;
+        while (list.size() > 1) {
+            int deleteIndex = (index + m - 1) % list.size();
+            list.remove(deleteIndex);
+            index = deleteIndex % list.size();
+        }
+
+        return list.get(0);
     }
 }
