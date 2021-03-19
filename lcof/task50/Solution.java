32eaa8fbc923abf9051d33af2f447da1d7f06ba5
syf <2654525303@qq.com>
2021-03-19 08:48:29 +0800

    prepare lcof 50

---
+++ lcof/task50/Solution.java
@@ -0,0 +1,10 @@
+package lcof.task50;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/">link</a>
+ */
+class Solution {
+    public char firstUniqChar(String s) {
+        return 0;
+    }
+}
---
+++ lcof/task50/Test1.java
@@ -0,0 +1,27 @@
+package lcof.task50;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1 {
+    @Test
+    public void example1() {
+        String s = "abaccdeff";
+
+        Solution solution = new Solution();
+        char expected = 'b';
+        char actual = solution.firstUniqChar(s);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void example2() {
+        String s = "";
+
+        Solution solution = new Solution();
+        char expected = ' ';
+        char actual = solution.firstUniqChar(s);
+        assertEquals(expected, actual);
+    }
+}
