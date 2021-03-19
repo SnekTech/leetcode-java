5731edc43562780010590de851055e23055ae9ed
syf <2654525303@qq.com>
2021-03-19 13:15:38 +0800

    finish lcof 61

--- README.md
+++ README.md
@@ -191,7 +191,7 @@ LeetCode(CN) solutions written in Java11.
 - [x] [59-1](lcof/task59_1/Solution.java)
 - [x] [59-2](lcof/task59_2/MaxQueue.java)
 - [x] [60](lcof/task60/Solution.java)
-- [ ] [61](lcof/task61/Solution.java)
+- [x] [61](lcof/task61/Solution.java)
 
 ## Awesome Tools
 
--- lcof/task61/Solution.java
+++ lcof/task61/Solution.java
@@ -1,10 +1,42 @@
 package lcof.task61;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @see <a href="https://leetcode-cn.com/problems/bu-ke-pai-zhong-de-shun-zi-lcof/">link</a>
  */
 class Solution {
     public boolean isStraight(int[] nums) {
-        return false;
+        Set<Integer> set = new HashSet<>();
+        int kings = 0;
+        int min = Integer.MAX_VALUE;
+        for (var num : nums) {
+            if (num == 0) {
+                kings++;
+            }
+            else {
+                min = Math.min(min, num);
+                if (!set.contains(num)) {
+                    set.add(num);
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        for (int i = 1; i < 5; i++) {
+            if (!set.contains(min + i)) {
+                if (kings > 0) {
+                    kings--;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
