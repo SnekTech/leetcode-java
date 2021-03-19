3ad2d612e8edeace0426cd54b3001065a0282ec6
syf <2654525303@qq.com>
2021-03-19 15:06:21 +0800

    finish lcof 63

--- lcof/task63/Solution.java
+++ lcof/task63/Solution.java
@@ -5,6 +5,22 @@ package lcof.task63;
  */
 class Solution {
     public int maxProfit(int[] prices) {
-        return 0;
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int n = prices.length;
+        int[] minHeight = new int[n];
+        minHeight[0] = prices[0];
+
+        for (int i = 1; i < n; i++) {
+            minHeight[i] = Math.min(prices[i], minHeight[i - 1]);
+        }
+        int maxProfit = 0;
+        for (int i = 1; i < n; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - minHeight[i - 1]);
+        }
+
+        return maxProfit;
     }
 }
